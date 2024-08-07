5ce8d9a4da1d59666f714c045f89c77e3b91bcf8
Pmiishra <sastri433@gmail.com>
2024-08-07 18:54:13 +0530

    sixth change

--- components/Recipelist/index.js
+++ components/Recipelist/index.js
@@ -8,43 +8,47 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+
 const audiowide = Audiowide({ subsets: ["latin"], weight: '400' });
 
 export default function Recipelist({ recipes_list }) {
-
     return (
-        <div >
+        <div>
             <div className={audiowide.className}>
-                <h1 className="text-4xl font-bold m-5 tracking-widest text-center" > RECIEPS </h1>
+                <h1 className="text-4xl font-bold m-5 tracking-widest text-center">RECIPES</h1>
             </div>
 
-            <div className="  grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 md:gap-8 md:m-10 " >
-                {
-                    recipes_list.map((recipeitems) => (
-                        <Card className="md:mx-0 mx-1 my-5">
-                            <CardHeader>
-                                <Link href={`/recipe/${recipeitems.id}`}>
-                                    <Image key={recipeitems.id} src={recipeitems.image} alt="this is image" width={500} height={100} className=" transition duration-500 ease-in-out transform-all  hover:scale-105 rounded" ></Image>
-                                </Link>
-                            </CardHeader>
-                            <CardContent>
-                                <Link href={`/recipe/${recipeitems.id}`}>
-                                    <CardTitle key={recipeitems.id} className="text-center">{recipeitems.name}</CardTitle>
-                                </Link>
-                                <CardDescription key={recipeitems.id} className="m-3 text-justify">{recipeitems.instructions[1]}</CardDescription>
-                            </CardContent>
-                            <CardFooter className="flex justify-between mx-4">
-                                <h5 key={recipeitems.id} className="font-bold">Rating: {recipeitems.rating} </h5>
-                                <h6 key={recipeitems.id} className="font-bold" > {recipeitems.mealType} </h6>
-                            </CardFooter>
-                        </Card>
-
-                    ))
-                }
-
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 md:gap-8 md:m-10">
+                {recipes_list.map((recipeitems) => (
+                    <Card key={recipeitems.id} className="md:mx-0 mx-1 my-5"> 
+                        <CardHeader>
+                            <Link href={`/recipe/${recipeitems.id}`}>
+                                <Image
+                                    src={recipeitems.image}
+                                    alt="this is image"
+                                    width={500}
+                                    height={100}
+                                    className="transition duration-500 ease-in-out transform-all hover:scale-105 rounded"
+                                />
+                            </Link>
+                        </CardHeader>
+                        <CardContent>
+                            <Link href={`/recipe/${recipeitems.id}`}>
+                                <CardTitle className="text-center">{recipeitems.name}</CardTitle>
+                            </Link>
+                            <CardDescription className="m-3 text-justify">
+                                {recipeitems.instructions[1]}
+                            </CardDescription>
+                        </CardContent>
+                        <CardFooter className="flex justify-between mx-4">
+                            <h5 className="font-bold">Rating: {recipeitems.rating}</h5>
+                            <h6 className="font-bold">{recipeitems.mealType}</h6>
+                        </CardFooter>
+                    </Card>
+                ))}
             </div>
         </div>
-
     );
 }
+// hum sirf child component ko key denge bas (yaha pe child compoent card hai jisme ki server data fetch hoga)
--- components/SingleRecipeList/index.js
+++ components/SingleRecipeList/index.js
@@ -9,69 +9,86 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+
 const audiowide = Audiowide({ subsets: ["latin"], weight: '400' });
 
 export default function SingleRecipeList({ single_recipes_list }) {
-
-
     return (
         <div className="pb-10 px-5">
             <div className={audiowide.className}>
-                <h1 className="text-4xl font-bold m-5 py-5 tracking-widest text-center captilize" key={single_recipes_list.id} > {single_recipes_list.name} </h1>
+                <h1 className="text-4xl font-bold m-5 py-5 tracking-widest text-center capitalize">
+                    {single_recipes_list.name}
+                </h1>
             </div>
 
-
-
-            <div className="md:flex  gap-10 justify-evenly">
-                <Card className="md:my-0 my-16">
+            <div className="md:flex gap-10 justify-evenly">
+                {/* card first */}
+                <Card key={single_recipes_list.id} className="md:my-0 my-16">
                     <CardHeader>
                         <Link href={`/recipe/${single_recipes_list.id}`}>
-                            <Image src={single_recipes_list.image} alt="this is image" width={500} height={100} className=" transition duration-500 ease-in-out transform-all  hover:scale-105 rounded" key={single_recipes_list.id} />
+                            <Image
+                                src={single_recipes_list.image}
+                                alt="Recipe image"
+                                width={500}
+                                height={100}
+                                className="transition duration-500 ease-in-out transform-all hover:scale-105 rounded"
+                            />
                         </Link>
                     </CardHeader>
                     <CardContent>
                         <Link href={`/recipe/${single_recipes_list.id}`}>
-                            <CardTitle key={single_recipes_list.id} className="text-center">{single_recipes_list.name}</CardTitle>
+                            <CardTitle className="text-center">{single_recipes_list.name}</CardTitle>
                         </Link>
-                        <CardDescription key={single_recipes_list.id} className="m-3 text-justify">{single_recipes_list.instructions[1]}</CardDescription>
+                        <CardDescription className="m-3 text-justify">
+                            {single_recipes_list.instructions[1]}
+                        </CardDescription>
                     </CardContent>
                     <CardFooter className="flex justify-between mx-4">
-                        <h5 key={single_recipes_list.id} className="font-bold">Rating : {single_recipes_list.rating} </h5>
-                        <h6 key={single_recipes_list.id} className="font-bold" > {single_recipes_list.mealType} </h6>
+                        <h5 className="font-bold">Rating: {single_recipes_list.rating}</h5>
+                        <h6 className="font-bold">{single_recipes_list.mealType}</h6>
                     </CardFooter>
                 </Card>
 
-                <Card>
-
+                {/* card second */}
+                <Card key={single_recipes_list.id}>
                     <CardHeader>
-                        <CardTitle key={single_recipes_list.id} className="text-center mb-10 mt-5 tracking-wider  "> {single_recipes_list.name}</CardTitle>
-                        <CardTitle key={single_recipes_list.id} className="text-center text-md tracking-wider"> Cuisine : {single_recipes_list.cuisine}</CardTitle>
+                        <CardTitle className="text-center mb-10 mt-5 tracking-wider">
+                            {single_recipes_list.name}
+                        </CardTitle>
+                        <CardTitle className="text-center text-md tracking-wider">
+                            Cuisine: {single_recipes_list.cuisine}
+                        </CardTitle>
                     </CardHeader>
 
                     <CardContent>
-                        <CardDescription key={single_recipes_list.id} className="m-3 mt-10 text-justify"><span className="underline">Ingredients</span> :  {single_recipes_list.ingredients[0]} , {single_recipes_list.ingredients[1]} , {single_recipes_list.ingredients[2]} , {single_recipes_list.ingredients[3]} , {single_recipes_list.ingredients[4]} , {single_recipes_list.ingredients[5]} .</CardDescription>
+                        <CardDescription className="m-3 mt-10 text-justify">
+                            <span className="underline">Ingredients</span>: {single_recipes_list.ingredients.join(", ")}
+                        </CardDescription>
 
+                        <CardDescription className="m-3 text-justify">
+                            <span className="underline">Prep Time (Minutes)</span>: {single_recipes_list.prepTimeMinutes}
+                        </CardDescription>
 
-                        <CardDescription key={single_recipes_list.id} className="m-3 text-justify md:mb-0 mb-10"> <span className="underline">Instructions</span> : {single_recipes_list.instructions[0]} , {single_recipes_list.instructions[1]} , {single_recipes_list.instructions[2]} , {single_recipes_list.instructions[3]} , {single_recipes_list.instructions[4]} ,  {single_recipes_list.instructions[5]}  </CardDescription>
+                        <CardDescription className="m-3 text-justify">
+                            <span className="underline">Cook Time (Minutes)</span>: {single_recipes_list.cookTimeMinutes}
+                        </CardDescription>
 
-                        <CardDescription key={single_recipes_list.id} className="m-3 text-justify"><span className="underline">PrepTimeMinutes</span> : {single_recipes_list.prepTimeMinutes}</CardDescription>
-
-                        <CardDescription key={single_recipes_list.id} className="m-3 text-justify"><span className="underline">CookTimeMinutes</span> : {single_recipes_list.cookTimeMinutes}</CardDescription>
-
-                        <CardDescription key={single_recipes_list.id} className="m-3 text-justify"><span className="underline">CaloriesPerServing</span> : {single_recipes_list.caloriesPerServing}</CardDescription>
-
-
+                        <CardDescription className="m-3 text-justify">
+                            <span className="underline">Calories Per Serving</span>: {single_recipes_list.caloriesPerServing}
+                        </CardDescription>
                     </CardContent>
-                    <CardFooter className="md:flex block justify-between md:mx-4 md:mt-8  ">
-                        <h5 key={single_recipes_list.id} className="font-bold md:text-left text-center my-2">Rating: {single_recipes_list.rating} </h5>
-                        <h6 key={single_recipes_list.id} className="font-bold md:text-right text-center my-2" > MealType : {single_recipes_list.mealType} </h6>
+                    <CardFooter className="md:flex block justify-between md:mx-4 md:mt-8">
+                        <h5 className="font-bold md:text-left text-center my-2">
+                            Rating: {single_recipes_list.rating}
+                        </h5>
+                        <h6 className="font-bold md:text-right text-center my-2">
+                            Meal Type: {single_recipes_list.mealType}
+                        </h6>
                     </CardFooter>
                 </Card>
-
             </div>
-
         </div>
-
     );
 }
+// hum sirf child component ko key denge bas (yaha pe child compoent 2 card hai jisme ki server data fetch hoga isliye hum dono ko key denge)
